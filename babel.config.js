e25bd0cb90964522593e3819ac89e4ab0444d060
moamachi22-source <moamachi22@gmail.com>
2026-05-28 13:21:43 +0330

    Update babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -4,15 +4,6 @@ module.exports = function (api) {
     presets: ['babel-preset-expo'],
     plugins: [
       'react-native-reanimated/plugin',
-      [
-        'module-resolver',
-        {
-          root: ['./'],
-          alias: {
-            '@': './src',
-          },
-        },
-      ],
     ],
   };
 };
